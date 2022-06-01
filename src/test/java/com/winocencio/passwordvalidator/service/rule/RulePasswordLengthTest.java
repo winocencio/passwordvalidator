6f8b17670be38d5fecdf3fff601f21db71b0b73e
Willian Inocencio <willian.i.alves@hotmail.com>
2022-06-01 00:38:55 -0300

    -FIX imports tests

--- src/test/java/com/winocencio/passwordvalidator/service/rule/RulePasswordLengthTest.java
+++ src/test/java/com/winocencio/passwordvalidator/service/rule/RulePasswordLengthTest.java
@@ -1,7 +1,7 @@
 package com.winocencio.passwordvalidator.service.rule;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
--- src/test/java/com/winocencio/passwordvalidator/service/rule/RulePasswordOneSpecialCharacterTest.java
+++ src/test/java/com/winocencio/passwordvalidator/service/rule/RulePasswordOneSpecialCharacterTest.java
@@ -1,6 +1,6 @@
 package com.winocencio.passwordvalidator.service.rule;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
